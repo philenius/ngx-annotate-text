4150f060c17092b0b098065ae96d605b3a8ff6f8
philenius <philenius@users.noreply.github.com>
2025-06-09 00:43:31 +0200

    refactor: use new @if() control flow syntax

--- projects/ngx-annotate-text/src/lib/ngx-annotate-text.module.ts
+++ projects/ngx-annotate-text/src/lib/ngx-annotate-text.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, NgComponentOutlet } from '@angular/common';
+import { NgComponentOutlet } from '@angular/common';
 import { NgxAnnotationRendererComponent } from './components/annotation-renderer/annotation-renderer.components';
 import { NgxAnnotateTextComponent } from './components/ngx-annotate-text/ngx-annotate-text.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NgComponentOutlet, NgxAnnotationRendererComponent, NgxAnnotateTextComponent],
+  imports: [NgComponentOutlet, NgxAnnotationRendererComponent, NgxAnnotateTextComponent],
   exports: [NgxAnnotationRendererComponent, NgxAnnotateTextComponent],
 })
 export class NgxAnnotateTextModule {}
